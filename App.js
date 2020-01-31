a6ab61e8f9fcdacffdb4d92bc020cd369982e4d7
AlsoufiFedaa <mlakahmd71@gmail.com>
2020-01-31 13:04:46 +0200

    adding firebase

--- App.js
+++ App.js
@@ -1,5 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyD47xlalTqy3QK4fFgfD9jImnI5prteLfw",
+  authDomain: "rentmeup.firebaseapp.com",
+  databaseURL: "https://rentmeup.firebaseio.com",
+  projectId: "rentmeup",
+  storageBucket: "rentmeup.appspot.com",
+  messagingSenderId: "259373225502",
+  appId: "1:259373225502:web:7e04123dc2de2f3e05000f",
+  measurementId: "G-K613V6TT4E"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return (
@@ -12,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
